97c1f94d4677dfab6a617d2e7af1275f6012cbca
JessicaPortilio <jessicaket91@gmail.com>
2023-02-22 19:49:44 -0300

    JavaScript

---
+++ JavaScript/Atividade para fixação do conteúdo/atividade.js
@@ -0,0 +1,34 @@
+/*
+1.O JavaScript é uma linguagem:
+a) Compilada
+b) Interpretada X
+
+Resposta:
+O JavaScript é uma linguagem interpretada, isso porque os scripts JS são 
+interpretados pelo browser em tempo de execução.
+
+2.O instrução "var" serve para:
+a) Indicar ao interpretador do JS que uma nova variável está sendo decladara X
+b) Forçar o tipo de variável.
+c) Atribuir valor a uma váriável
+
+Resposta: Embora opcional, a instrução "var" 
+serve para declarar de forma explicita a criação de uma variável.
+
+3. Selecione qual instrução é responsável por gerar uma saída no 
+console do browser (ferramentas de desenvolvedor do browser)
+a) alert()
+b) document.write()
+c) console.log() X
+
+4. O valor null atribuído a uma variável indica que:
+a) A variável representa a ausência intencional de um valor x
+b) A variável representa o valor undefined
+c) A variável não pode ter seu valor modificado
+
+5. Comentários servem, dentre outras coisas, para:
+a) Permite capturar entrada de textos do usuário para uso dentro da lógica do script
+b) Exibir mensagens customizdas para o usuário
+c) Inclusão de textos descritivos que são ignorados pelo interpretador do JS e ajudam
+na identificação da lógica da programação facilitando futuras manutenções. x
+*/
---
+++ JavaScript/Variáveis/variaveis.js
@@ -0,0 +1,55 @@
+/*
+
+Variáveis - Introdução
+
+O que são variáveis?
+
+				   string
+				  /
+Tipos de variáveis - number
+				  \
+				   boolean
+
+Regras para declaração de variáveis
+
+	Não podem ser iniciadas com números, apenas com letras ou "_"
+	Não podem ser uutilizados caracteres especiais como "ç", "^", "~"
+	Não podem ser iguais as palavras reservadas da linguagem
+
+Declarando variáveis com a instrução "var"
+
+Case sensitive
+	var curso, var Curso e var CURSO são variáveis diferentes
+
+Exemplos
+	var nome, var AulaVariaveis, var _Idade // pode
+	var 123nome, var número // não pode 
+
+*/
+
+
+/*
+Alterando valores de variáveis
+
+var nome = 'Maria'
+
+var total = 755.76
+*/
+
+/*
+Revisão
+
+Interpretador JavaScript
+var texto = 'Curso de JavaScript'
+document.write('<h1> ' + texto + ' </h1>')
+
+
+vai aparecer lá na tela Curso de Java Script
+
+Inclusão de código JavaScript
+Comentários
+Concatenação
+Precedência de execução
+Variáveis
+*/
+
