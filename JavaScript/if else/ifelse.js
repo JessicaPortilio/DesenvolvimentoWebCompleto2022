235779526960bf6be7322b2a480a64d0f50c43b6
JessicaPortilio <jessicaket91@gmail.com>
2023-02-23 16:02:50 -0300

    JavaScript

---
+++ JavaScript/if else/ifelse.js
@@ -0,0 +1,21 @@
+/*
+
+if/else parte 1 - introdução
+
+if(condição){
+	//trecho de código que será executado
+}else{
+	//trecho de código que será executado
+}
+
+----------------------------------------
+if(condição){
+	//trecho de código que será executado
+}else if{
+	//trecho de código que será executado
+}
+else{
+	//trecho de código que será executado
+}
+
+*/
