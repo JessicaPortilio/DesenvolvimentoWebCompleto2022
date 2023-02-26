53fa9d17fe6545fc846fa4c7a29ae74814dd759c
JessicaPortilio <jessicaket91@gmail.com>
2023-02-25 21:44:55 -0300

    JavaScript

---
+++ JavaScript/Eventos/eventos.js
@@ -0,0 +1,28 @@
+/*
+
+Mouse
+
+onclick (Aciona quando o elemento é clicado)
+ondblclick (Aciona quando o elemento é clicado duas vezes)
+onmouseup (Aciona quando o click em um determinado evento é liberado)
+onmouseover (Aciona quando o cursor do mouse fica sobre um elemento)
+onmouseout (Aciona quando o cursor sai da página onde o elemento está sendo ocupado)
+
+Teclado
+
+onkeydown (Acionada quando uma tecla é pressionada)
+onkeypress (Acionada quando uma tecla é liberada)
+onkeyup (Acionada quando eu mantenho a tecla pressionada)
+
+Janela
+
+onresize (Aciona quando a janela é redimensionada)
+onscroll
+
+Formulário
+
+onfocus 
+onblur 
+onchange 
+
+*/
