ea0557649b9ca2b3d0c32be81fac03a43c599dbd
JessicaPortilio <jessicaket91@gmail.com>
2023-02-27 20:02:05 -0300

    JavaScript

---
+++ JavaScript/DOM/dom.js
@@ -0,0 +1,31 @@
+/*
+
+DOM
+
+O que é DOM?
+ODCUMENT OBJECT MODEL
+
+API que permite o acesso via JavaScript aos elementos da página
+
+
+Selecionando elementos da página
+
+document(API DOM)
+getElementById()
+getElementsByTagName()
+getElementsByClassNome()
+getElementsByName()
+
+<input
+	type="text"
+	placeholder="Digite seu nome"
+	id="nome_usuario"
+	class="campo_texto"
+	name="nome"
+/>
+
+Manipulando valores de inputs (text)
+
+Verifica o caracter digitado, se letra vai para o campo "letras" se número vai para o campo "Números"
+
+*/
