94efe6b988214d5cf89a81da6e5852b64afe2c72
JessicaPortilio <jessicaket91@gmail.com>
2023-03-03 22:21:59 -0300

    JavaScript

---
+++ JavaScript/Array/array.js
@@ -0,0 +1,35 @@
+/*
+
+Array básico
+O que é um array?
+
+Lita de frutas
+1 banana
+2 maçã
+3 morango
+4 uva
+
+Com variáveis
+var variavel_1 = 'Banana'
+var variavel_2 = 'Maçã'
+var variavel_3 = 'Morango'
+var variavel_4 = 'Uva'
+
+
+Com um único array
+var lista_frutas = Array();
+lista_frutas[1] = 'Banana'
+lista_frutas[2] = 'Maçã'
+lista_frutas[3] = 'Morango'
+lista_frutas[4] = 'Uva'
+
+Array multidimensional
+O que é um array multidimensional
+
+Lista de coisas
+frutas  Array()              1 Banana                1 João							
+pessoas Array()              2 Maçã                  2 José
+							 3 Morango               3 Maria
+							 4 Uva
+
+*/
---
+++ JavaScript/BOM/bom.js
@@ -0,0 +1,20 @@
+/*
+
+BOM
+
+O que é BOM?
+Browser Object Model
+API que permite o acesso via JavaScript o recursos do browser
+
+https://www.w3schools.com/jsref/obj_window.asp
+
+Timing
+
+//Executa ação uma vez após o tempo informado
+setTimeout(<ação>, <tempo em milissegundos>) 
+clearTimeout()
+
+// Sempre executa a ação após o período informado
+setInterval(<ação>, <tempo em milissegundos>) // Sempre executa a ação após o período informado
+clearInterval()
+*/
